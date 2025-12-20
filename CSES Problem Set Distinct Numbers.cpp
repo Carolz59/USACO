293557d84bf1382084ef3228653a288e432a6c39
CarolDoesntHateCoding <kainxtgen@gmail.com>
2025-12-20 01:36:52 -0800

    Create CSES Problem Set Distinct Numbers.cpp
    Implement distinct number counting using vector and sorting.

---
+++ CSES Problem Set Distinct Numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+// vector method
+ 
+    int n;
+    cin>>n;
+    vector<int>v;
+    for (int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+ 
+    int count=1;
+    for (int i=1;i<n;i++){
+        if (v[i]!=v[i-1]){
+            count++;
+        }
+    }
+ 
+    cout<<count;
+ 
+ 
+ 
