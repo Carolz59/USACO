87c5aded9b84790bde343e11fa960670ca1511b2
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-02-01 13:12:13 -0800

    Refactor prime checking and input handling

--- CCC '19 S2 - Pretty Average Primes.cpp
+++ CCC '19 S2 - Pretty Average Primes.cpp
@@ -1,61 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <set>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-//(a+b)/2=x
-
-bool isprime(int x){
+bool prime(int x){
     if (x==2){
         return true;
-    }else if(x%2==0&&x!=2){
+
+    }else if(x<2){
         return false;
     }
 
-    for (int i=3;i<=sqrt(x);i+=2){
+    for (int i=2;i*i<=x;i++){
         if (x%i==0){
             return false;
         }
     }
     return true;
-}
 
-pair<int, int> avprime(int x){
-    int tot=x*2;
-    //check 2
-    if (isprime(tot-2)){
-        return make_pair(2,tot-2);
-    }
-    for (int i=3;i<x;i++){
-        if (isprime(i)&&isprime(tot-i)){
-            return make_pair(i,tot-i);
+
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<pair<int,int>>ans;
+
+    for (int i=0;i<t;i++){
+        int n;
+        cin>>n;
+
+        int tot=n*2;
+
+        for (int i=2;i<=tot;i++){
+            if (prime(i)&&prime(tot-i)){
+                ans.push_back({i,tot-i});
+                break;
+                
+            }
+
         }
     }
-    return make_pair(0, 0);
-}
-
-
-int main(){
-    int n;
-    cin>>n;
-    vector<int>nums;
-    int a;
-
-    for (int i=0;i<n;i++){
-        cin>>a;
-        nums.push_back(a);
+    for (auto [a,b]:ans){
+        cout<<a<<" "<<b<<endl;
     }
-
-    //vector<pair<int,int>> results;
-
-
-    for (int i=0;i<n;i++){
-        cout<<avprime(nums[i]).first<<" "<<avprime(nums[i]).second<<endl;
-    }
-
-
+    
 
 }
