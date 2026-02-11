5ef2172c10e4bf2f746043f1f97e450f926fd3f2
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-02-11 14:47:33 -0800

    Update VM7WC '16 #5 Gold - Jayden Studies Trees.cpp

--- VM7WC '16 #5 Gold - Jayden Studies Trees.cpp
+++ VM7WC '16 #5 Gold - Jayden Studies Trees.cpp
@@ -1,55 +1,55 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-pair<int,int> leng(int start,vector<vector<int>>&adj,vector<int>&visited){
+int n;
+
+vector<int>dist;
+vector<vector<int>>adj;
+pair<int,int> bfs(int s){
+    dist.assign(n+1,-1);
     queue<int>q;
-    q.push(start);
-    visited[start]=0;
+    q.push(s);
+    dist[s]=0;
+    int endnode;
+    int enddist=0;
 
     while (!q.empty()){
         int cur=q.front();
         q.pop();
 
-        for (int i=0;i<adj[cur].size();i++){
-            int nei=adj[cur][i];
-            if (visited[nei]==-1){
-                q.push(nei);
-                visited[nei]=visited[cur]+1;
+        for (auto u:adj[cur]){
+            if (dist[u]==-1){
+                dist[u]=dist[cur]+1;
+                q.push(u);
+                if (dist[u]>enddist){
+                    enddist=dist[u];
+                    endnode=u;
+                }
             }
         }
     }
-    int max=0;
-    int index;
-    for (int i=0;i<visited.size();i++){
-        if (visited[i]>max){
-            max=visited[i];
-            index=i;
-        }
-    }
-    return make_pair(max,index);
+
+    return {endnode,enddist};
+
+
 }
 int main(){
-    int n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     cin>>n;
-    int a,b;
-    vector<vector<int>>adj(n+1);
-    vector<int>visited(n+1,-1);
+    adj.assign(n+1,vector<int>());
+
     for (int i=0;i<n-1;i++){
+        int a,b;
         cin>>a>>b;
+
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    int start=1;
 
-    int n1=leng(start,adj,visited).second;
-    visited.assign(n+1,-1);
+    auto [targ,di]=bfs(1);
+    auto [targ2,dia]=bfs(targ);
 
-    int n2=leng(n1,adj,visited).first;
+    cout<<dia<<endl;
 
-
-    cout<<n2<<endl;
-
-
-    
 }
