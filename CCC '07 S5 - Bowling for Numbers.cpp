cd334393ea49689e96fb4dcb01609ab90ee497b3
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-02-12 21:58:37 -0800

    Create CCC '07 S5 - Bowling for Numbers.cpp

---
+++ CCC '07 S5 - Bowling for Numbers.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int q;
+    cin>>q;
+    int n,b,w;
+
+    for (int z=0;z<q;z++){
+        
+        
+        cin>>n>>b>>w;
+        vector<int>pins(n+1,0),pref(n+1,0);
+
+        for (int i=1;i<=n;i++){
+            cin>>pins[i];
+            pref[i]=pref[i-1]+pins[i];
+
+        }
+        vector<vector<int>>dp(n+1,vector<int>(b+1,0));
+        dp[0][0]=0;
+        for (int i=1;i<=b;i++){
+            for (int j=1;j<=n;j++){
+
+                int news=0;
+                int skip=dp[j-1][i];
+                if (j>=w){
+                    int seg=pref[j]-pref[j-w];
+                    news=seg+dp[j-w][i-1];
+                }
+
+                dp[j][i]=max(skip,news);
+            
+
+        }
+        
+    }
+    cout<<dp[n][b]<<endl;
+
+    
+    }
+        
+
+
+    
+}
