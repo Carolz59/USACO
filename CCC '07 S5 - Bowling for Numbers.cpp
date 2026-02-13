38f1cf9b5ab27b2f1e543e5276ef6a0959ee18d5
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-02-12 22:09:40 -0800

    Update CCC '07 S5 - Bowling for Numbers.cpp

--- CCC '07 S5 - Bowling for Numbers.cpp
+++ CCC '07 S5 - Bowling for Numbers.cpp
@@ -9,40 +9,33 @@ int main(){
     int n,b,w;
 
     for (int z=0;z<q;z++){
-        
-        
         cin>>n>>b>>w;
-        vector<int>pins(n+1,0),pref(n+1,0);
+        vector<int>pins(n+1,0);
+        vector<int>pref(n+1,0);
+        vector<vector<int>>dp(n+1,vector<int>(b+1,0));
 
         for (int i=1;i<=n;i++){
             cin>>pins[i];
             pref[i]=pref[i-1]+pins[i];
-
         }
-        vector<vector<int>>dp(n+1,vector<int>(b+1,0));
-        dp[0][0]=0;
+
         for (int i=1;i<=b;i++){
             for (int j=1;j<=n;j++){
-
-                int news=0;
                 int skip=dp[j-1][i];
+                int news=0;
+
                 if (j>=w){
                     int seg=pref[j]-pref[j-w];
                     news=seg+dp[j-w][i-1];
                 }
-
                 dp[j][i]=max(skip,news);
-            
-
+            }
         }
-        
-    }
-    cout<<dp[n][b]<<endl;
 
-    
-    }
-        
+        cout<<dp[n][b]<<endl;
 
 
     
+}
+
 }
