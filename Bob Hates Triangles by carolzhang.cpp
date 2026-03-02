4eb1c7e07041679e85246ad726aadb3f7d227cf0
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-03-01 23:56:03 -0800

    Add main function to handle edge input and output
    Implement main function to read and output edges.

---
+++ Bob Hates Triangles by carolzhang.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n,m;
+    cin>>n>>m;
+    for (int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        if (u>v){
+            swap(u,v);
+        }
+
+        cout<<u<<" "<<v<<endl;
+    }
+
+    
+}
