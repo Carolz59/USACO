88c406a4be2287c8b085d063b4e738d7439c76cf
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-05-20 14:50:34 -0700

    Create GFSSOC '15 Winter S1 - OR-deal.cpp

---
+++ GFSSOC '15 Winter S1 - OR-deal.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin>>n;
+
+    string output="";
+    while (n>0){
+        output+="1";
+        n/=2;
+
+        
+    }
+    cout<<output;
+
+    
+
+
+
+}
