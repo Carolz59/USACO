029a82a91c8a3ec515a8944090f23e0adc11b7f3
CarolDoesntHateCoding <kainxtgen@gmail.com>
2026-05-31 11:47:38 -0700

    Create The Odd Number.cpp

---
+++ The Odd Number.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+using namespace std;
+
+int main(){
+    //ios::sync_with_stdio(false);
+    //cin.tie(nullptr);
+
+    int n;
+    scanf("%d",&n);
+
+    int ans=0;
+    while (n--){
+        int x;
+        scanf("%d",&x);
+        ans^=x;
+
+        
+    }
+
+    printf("%d\n",ans);
+
+
+
+        
+}
+
